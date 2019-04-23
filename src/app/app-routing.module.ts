81abadfdd5d8563dc18721afde0443113eb068f0
jeanstkng <jjg.castillo@yavirac.edu.ec>
2019-04-23 00:50:38 -0500

    Template listo CR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FirstComponent } from './pages/items/first/first.component';
 
-const routes: Routes = [];
+// tslint:disable-next-line: variable-name
+const app_routes: Routes = [
+
+  {path: '', component: PortafolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'first', component: FirstComponent},
+  {path: '**', pathMatch: 'full', redirectTo: ''}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FirstComponent } from './pages/items/first/first.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
