9551a19a21f30650361ff40c93c07c82bf06bf19
jeanstkng <jjg.castillo@yavirac.edu.ec>
2019-04-23 17:53:10 -0500

    Fin seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,15 @@ import { FirstComponent } from './pages/items/first/first.component';
 // tslint:disable-next-line: variable-name
 const app_routes: Routes = [
 
-  {path: '', component: PortafolioComponent},
+  {path: 'home', component: PortafolioComponent},
   {path: 'about', component: AboutComponent},
   {path: 'first', component: FirstComponent},
-  {path: '**', pathMatch: 'full', redirectTo: ''}
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(app_routes)],
+  imports: [RouterModule.forRoot(app_routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
